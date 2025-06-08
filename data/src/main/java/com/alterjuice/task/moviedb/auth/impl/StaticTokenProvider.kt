cc9721f8d02418cd0466d2c4a033ebad0df6a6dd
BohdanSnurnitsyn <alterjuice1758@gmail.com>
2025-06-08 15:46:53 +0300

    Fix StaticAuthProvider injection

--- data/src/main/java/com/alterjuice/task/moviedb/auth/impl/StaticTokenProvider.kt
+++ data/src/main/java/com/alterjuice/task/moviedb/auth/impl/StaticTokenProvider.kt
@@ -2,8 +2,9 @@ package com.alterjuice.task.moviedb.auth.impl
 
 import com.alterjuice.task.moviedb.BuildConfig
 import com.alterjuice.task.moviedb.auth.TokenProvider
+import javax.inject.Inject
 
-internal class StaticAuthProvider: TokenProvider {
+internal class StaticAuthProvider @Inject constructor(): TokenProvider {
     override suspend fun getToken(): String? {
         return BuildConfig.TMDB_API_TOKEN
     }
