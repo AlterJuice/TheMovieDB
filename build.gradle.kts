7b0ed30cb6e2aa221282d0d91739dc56881658fe
BohdanSnurnitsyn <alterjuice1758@gmail.com>
2025-06-10 02:42:24 +0300

    Added configuration to make compose-reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,14 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
 }
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += listOf(
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${rootProject.buildDir}/compose_metrics"
+            )
+        }
+    }
+}
