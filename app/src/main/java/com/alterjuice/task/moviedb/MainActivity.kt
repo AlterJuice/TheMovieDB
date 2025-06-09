b49317cf069f1bc0ce26e37d00e9298acd780e64
BohdanSnurnitsyn <alterjuice1758@gmail.com>
2025-06-09 21:31:45 +0300

    fix movies posters loading freezes
    created dependency of ImageLoader
    Updated size parameter for posters in POSTER_BASE_URL

--- app/src/main/java/com/alterjuice/task/moviedb/MainActivity.kt
+++ app/src/main/java/com/alterjuice/task/moviedb/MainActivity.kt
@@ -5,30 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import coil.ImageLoader
 import com.alterjuice.task.moviedb.core.ui.theme.TheMovieDBTheme
 import com.alterjuice.task.moviedb.feature.movies.ui.screens.MoviesScreen
 import com.alterjuice.task.moviedb.feature.movies.ui.utils.LocalImageLoader
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TheMovieDBTheme {
-                val context = LocalContext.current
-                val imageLoader = remember {
-                    ImageLoader.Builder(context).crossfade(true).build()
-                }
                 CompositionLocalProvider(
                     LocalImageLoader provides imageLoader
                 ) {
@@ -38,21 +34,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheMovieDBTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/alterjuice/task/moviedb/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.alterjuice.task.moviedb.di
+
+import android.content.Context
+import coil.ImageLoader
+import coil.request.CachePolicy
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.asCoroutineDispatcher
+import java.util.concurrent.Executors
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object AppModule {
+
+    @Provides
+    @Singleton
+    internal fun provideImageLoader(
+        @ApplicationContext context: Context,
+    ): ImageLoader {
+        return ImageLoader.Builder(context)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .dispatcher(Executors.newFixedThreadPool(2).asCoroutineDispatcher())
+            .crossfade(true)
+            .build()
+    }
+}
