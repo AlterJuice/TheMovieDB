0d3fa3ec3936fc46b3f80ad1811ba50fe4667fb3
BohdanSnurnitsyn <alterjuice1758@gmail.com>
2025-06-10 02:52:01 +0300

    Fix API request filter parameters

--- data/src/main/java/com/alterjuice/task/moviedb/network/service/MovieApiServiceImpl.kt
+++ data/src/main/java/com/alterjuice/task/moviedb/network/service/MovieApiServiceImpl.kt
@@ -10,12 +10,20 @@ internal class MovieApiServiceImpl(
     private val httpClient: HttpClient
 ) : MovieApiService {
     companion object {
-        const val DISCOVER_MOVIE_PATH = "discover/movie"
+        private const val DISCOVER_MOVIE_PATH = "discover/movie"
+        private const val VOTE_AVERAGE_PARAM = "vote_average.gte"
+        private const val VOTE_COUNT_PARAM = "vote_count.gte"
+        private const val SORT_BY_PARAM = "sort_by"
+        private const val PAGE_PARAM = "page"
+
     }
 
     override suspend fun getMovies(page: Int): MoviesResponseDto {
         return httpClient.get(DISCOVER_MOVIE_PATH) {
-            parameter("page", page)
+            parameter(PAGE_PARAM, page)
+            parameter(VOTE_AVERAGE_PARAM, 7)
+            parameter(VOTE_COUNT_PARAM, 100)
+            parameter(SORT_BY_PARAM, "primary_release_date.desc")
         }.body()
     }
 }
