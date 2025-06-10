e5d9860aa4efc77140fbd58886a248132e3c1b01
BohdanSnurnitsyn <alterjuice1758@gmail.com>
2025-06-10 12:02:06 +0300

    Removed unused property

--- buildSrc/src/main/kotlin/Properties.kt
+++ buildSrc/src/main/kotlin/Properties.kt
@@ -7,11 +7,6 @@ object MovieDBServiceProps: PropsFile {
     override val filePath: String get() = defaultPropertiesFilePath()
     val BASE_URL = PropsKey.Single("TMDB_BASE_URL")
 
-    val API_KEY = PropsKey.BuildTypeBased(
-        generalKey = "TMDB_API_KEY",
-        debug = "TMDB_DEBUG_API_KEY",
-        release = "TMDB_RELEASE_API_KEY",
-    )
     val API_TOKEN = PropsKey.BuildTypeBased(
         generalKey = "TMDB_API_TOKEN",
         debug = "TMDB_DEBUG_API_TOKEN",
